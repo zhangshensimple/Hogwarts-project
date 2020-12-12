d29a51ff7f51ba9ee59249415ff06a712dd138c7
zhangshensimple <zhangshensimple@foxmail.com>
2020-12-12 18:48:25 +0800

    '测试代码提交'

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+class Calculator:
+    def add(self,a,b,c):
+        return a+b+c
+    def sub(self,a,b):
+        return a-b
+    def multi(self,a,b):
+        return a*b
+    def div(self,a,b):
+        return a/b
